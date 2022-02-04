48d7550623df8e8b89951a58c37bdbec08bc6c43
wxyz03002 <wxyz03002@naver.com>
2022-02-04 15:28:15 +0900

    final_upload

--- YOLOv3/Detect_exe.bat
+++ YOLOv3/Detect_exe.bat
@@ -1,2 +1,2 @@
 cd c:\a3c_timberman\yolov3
-call "%userprofile%\\anaconda3\\scripts\\activate.bat" ai & python detect_5.py --epsilon 0.001 --epsilon_discount 0.001 --learning_rate 0.001 --node 32 --step_mode 1 --batch_size 8
+call "%userprofile%\\anaconda3\\scripts\\activate.bat" ai & python detect_5.py --epsilon 1.0 --epsilon_discount 0.001 --learning_rate 0.001 --node 32 --step_mode 1 --batch_size 8
